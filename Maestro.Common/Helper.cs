c36854f76e0100ef093cf69359a088e46a266ce7
Otatiaro <contact@digital-consulting.fr>
2020-11-04 08:41:56 +0100

    Add  Brotli compression methods for device database compression

--- Maestro.Common/Helper.cs
+++ Maestro.Common/Helper.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using Brotli;
 
 namespace Maestro.Common
 {
@@ -12,6 +14,32 @@ namespace Maestro.Common
                 yield return "cannot be empty";
             if (input.Contains(" "))
                 yield return "cannot contain whitespace (replace by underscore or camel case)";
+        }
+
+        public static byte[] Compress(this byte[] data)
+        {
+            using (var msInput = new System.IO.MemoryStream(data))
+            using (var msOutput = new System.IO.MemoryStream())
+            using (var bs = new BrotliStream(msOutput, System.IO.Compression.CompressionMode.Compress))
+            {
+                bs.SetQuality(11);
+                bs.SetWindow(22);
+                msInput.CopyTo(bs);
+                bs.Close();
+                return msOutput.ToArray();
+            }
+        }
+
+        public static byte[] Decompress(this byte[] data)
+        {
+            using (var msInput = new System.IO.MemoryStream(data))
+            using (var bs = new BrotliStream(msInput, System.IO.Compression.CompressionMode.Decompress))
+            using (var msOutput = new System.IO.MemoryStream())
+            {
+                bs.CopyTo(msOutput);
+                msOutput.Seek(0, System.IO.SeekOrigin.Begin);
+                return msOutput.ToArray();
+            }
         }
     }
 }
